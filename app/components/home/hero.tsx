45e28626736e85c2d4c910686188311794eef0c4
Victorola <victoluolatunji@gmail.com>
2024-10-16 21:20:13 +0100

    recent episodes

---
+++ app/components/home/hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className="text-center w-full md:max-w-[794px] mx-auto md:text-[78px] md:leading-[90px] tracking-[-3.12px] text-main font-satBold font-bold">
+        we’re
+        <span className="text-primary italic font-bold font-satoshiBold">
+          *bullish*
+        </span>{" "}
+        on first time & early stage builders
+      </h1>
+    </div>
+  );
+}
