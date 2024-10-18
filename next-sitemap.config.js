50912f80bc9b0a510cea609329c3265be5471a57
Victorola <victoluolatunji@gmail.com>
2024-10-18 04:04:01 +0100

    🎉 [chore]: seo ups

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl:
+    "https://www.buildforge.xyz" ||
+    "https://buildforge.xyz" ||
+    "buildforge.xyz",
+  changefreq: "weekly",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
