0ed454c3cd491c1cf20f10af8ad6d5d9a6a72c6c
Victorola <victoluolatunji@gmail.com>
2024-10-18 04:23:13 +0100

    🔧 [images]

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        port: "",
+        protocol: "https",
+        hostname: "cdn.dropp.cloud",
+        pathname: "/**",
+      },
+      {
+        port: "",
+        protocol: "https",
+        hostname: "randomuser.me",
+        pathname: "/api/portraits/**",
+      },
+    ],
+  },
+  env: {
+    EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || "fuckoff",
+  },
+};
 
 export default nextConfig;
