dec2d5c2d28176273ed7465868c221ce65fae69c
Victorola <victoluolatunji@gmail.com>
2024-10-19 12:22:28 +0100

    [fix builderror

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,37 +1,34 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    const { email } = req.body;
-    try {
-      // Check if the email already exists in the database
-      const existingSubscriber = await prisma.subscriber.findUnique({
-        where: { email },
-      });
+export async function POST(req: NextRequest) {
+  try {
+    const { email } = await req.json();
 
-      if (existingSubscriber) {
-        return res.status(409).json({ error: "Email already subscribed" });
-      }
+    // Check if the email already exists in the database
+    const existingSubscriber = await prisma.subscriber.findUnique({
+      where: { email },
+    });
 
-      // Create a new subscriber
-      const newSubscriber = await prisma.subscriber.create({
-        data: {
-          email,
-        },
-      });
-      res.status(201).json(newSubscriber);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error: "Failed to subscribe" });
+    if (existingSubscriber) {
+      return NextResponse.json(
+        { error: "Email already subscribed" },
+        { status: 409 }
+      );
     }
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    // Create a new subscriber
+    const newSubscriber = await prisma.subscriber.create({
+      data: {
+        email,
+      },
+    });
+
+    return NextResponse.json(newSubscriber, { status: 201 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Failed to subscribe" }, { status: 500 });
   }
 }
