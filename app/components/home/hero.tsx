22392592b13fd0f34ae41e53d98007ff4b79568e
Victorola <victoluolatunji@gmail.com>
2024-12-09 23:05:45 +0100

    fixes

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -4,7 +4,7 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function Hero() {
   return (
-    <div className="mb-[72px] gap-[72px]">
+    <div className="mb-[72px]">
       <h1 className="text-center text-[40px] leading-[48px] mb-[45px]_ tracking-[-1.6px] max-w-[386px] md:max-w-[794px] md:mb-[77px]_ mx-auto md:text-[78px] md:leading-[90px] md:tracking-[-3.12px] text-main font-satBold font-bold">
         an{" "}
         <span className="text-primary font-bold font-satBoldI">
@@ -21,7 +21,7 @@ export default function Hero() {
           />
         </div>
       </h1>
-      <Button className="!bg-transparent border-[#232328] border-[1.5px]">
+      <Button className="mx-auto mt-[72px] block self-center !bg-transparent border-[#232328] border-[1.5px]">
         join the waitlist
       </Button>
     </div>
